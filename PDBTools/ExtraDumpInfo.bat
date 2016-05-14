eeedb678e8cfc57f8ecc174cf0670830e08309dc
sammylan <sammylan234@gmail.com>
2016-05-14 19:55:17 +0800

    不设置代理

--- PDBTools/ExtraDumpInfo.bat
+++ PDBTools/ExtraDumpInfo.bat
@@ -4,10 +4,9 @@
 @set _NT_ALT_SYMBOL_PATH=%_NT_ALT_SYMBOL_PATH%
 @set _NT_SOURCE_PATH=%_NT_SOURCE_PATH%;SRV*D:\HummerSrcCache
 @set _NT_SYMBOL_PATH=%_NT_SYMBOL_PATH%;srv*D:\symbols\MicrosoftPublicSymbols*https://msdl.microsoft.com/download/symbols;srv*D:\symbols*\\tencent.com\tfs\部门目录\SNG社交网络事业群\社交平台部\微云\windows\PDB\FinalRelease
-@rem set  _NT_SYMBOL_PROXY=113.108.72.148:8088
+@set  _NT_SYMBOL_PROXY=
 
 
 @set dump_file=%1
 
 @call windbg.exe -c  "kc;q" -loga "dump.log" -z %dump_file%
-@cmd
