d27221af42643fabe838a65edb3219bd149af3ff
sammylan <sammylan@gmail.com>
2016-07-19 12:53:48 +0800

    update

--- build3rd_git/buildBoost.bat
+++ build3rd_git/buildBoost.bat
@@ -7,5 +7,8 @@
 @rem call %BOOST_DIR%b2 headers >>%LOGS_DIR%\buildBoost.log
 
 @rem Брвы
+@set BUILD_CONF=install --prefix=%BULID_DIR%boost --includedir=%LIB3rd%  --libdir=%LIB3rd%Lib toolset=msvc-%VS_VERSION%.0 runtime-link=static
+@call %BOOST_DIR%b2  %BUILD_CONF%>>%LOGS_DIR%\buildBoost.log
+
 @set BUILD_CONF=install --prefix=%BULID_DIR%boost --includedir=%LIB3rd%  --libdir=%LIB3rd%Lib toolset=msvc-%VS_VERSION%.0
 @call %BOOST_DIR%b2  %BUILD_CONF%>>%LOGS_DIR%\buildBoost.log
